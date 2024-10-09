94940bdfcd9a7ac8321ec9fa8f727373d500f339
hubwriter <hubwriter@github.com>
2024-10-09 12:50:37 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const greetingDiv = document.getElementById('greeting');
+    
+    // Vulnerable to XSS
+    greetingDiv.innerHTML = `Hello, ${username}!`;
+});
