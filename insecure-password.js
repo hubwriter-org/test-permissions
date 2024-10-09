20e1c4eb1cb6948444f8a31f24a1a153b62ccdcf
hubwriter <hubwriter@github.com>
2024-10-09 20:04:52 +0100

    Fix comment punctuation in insecure-password.js

--- insecure-password.js
+++ insecure-password.js
@@ -1,5 +1,5 @@
 function insecurePassword() {
-  // GOOD: the random suffix is cryptographically secure
+  // GOOD: the random suffix is cryptographically secure.
   var suffix = window.crypto.getRandomValues(new Uint32Array(1))[0];
   var password = "sss" + suffix;
   return password;
