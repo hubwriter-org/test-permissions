3113d087a396e082caffca56e128dbefd7384883
hubwriter <hubwriter@github.com>
2024-10-10 12:42:27 +0100

    Change server port to 3030

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000;
+const port = 3030;
 
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
